f9fb3ba66af17c55c74d847cc465111b079f4e4e
unknown <larissalmacedo@gmail.com>
2021-08-07 10:16:41 -0300

    Ajustes no código

--- main.cpp
+++ main.cpp
@@ -2,10 +2,17 @@
 
 using namespace std;
 
+struct pessoa
+{
+	string nome;
+	int idade;
+};
+
 int main (){
 	cout << "Aula de GIT";
 	cout << "Digite seu nome";
 	string nome;
 	cin >> nome;
 	cout << "Seja bem vindo " << nome;
+	cout << "Teste";
 }
