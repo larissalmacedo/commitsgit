2bfa06e6135291029530475fa9891c942b6dec62
unknown <larissalmacedo@gmail.com>
2021-08-07 10:25:29 -0300

    commit

--- main.cpp
+++ main.cpp
@@ -10,9 +10,12 @@ struct pessoa
 
 int main (){
 	cout << "Aula de GIT";
+<<<<<<< HEAD
 	cout << "Digite seu nome";
 	string nome;
 	cin >> nome;
 	cout << "Seja bem vindo " << nome;
 	cout << "Teste";
+=======
+>>>>>>> parent of 6597303 (Alteração no projeto para perguntar o nome)
 }
